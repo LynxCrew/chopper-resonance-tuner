f3532b74cea7ead18264448f4fb9fc5cc7aa16bd
Zeanon <thezeanon@gmail.com>
2024-05-02 10:47:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,56 +29,6 @@ cfg_incl_path=~/printer_data/config/printer.cfg
 mkdir -p "${cfg_path}"
 ln -sf "$repo_path/$cfg_name" $cfg_path
 
-if [ -f "$cfg_incl_path" ]; then
-    if ! grep -q "^\[include $cfg_name\]$" "$cfg_incl_path"; then
-	    read -p " Do you want to automatically include the config? (y/n): " answer
-		if [ "$answer" != "${answer#[Yy]}" ]; then
-			sudo service klipper stop
-			sed -i "1i\[include $cfg_name]" "$cfg_incl_path"
-			# echo "Including $cfg_name to $cfg_incl_path successfully complete"
-			sudo service klipper start
-		fi
-    else
-        echo "Including $cfg_name aborted, $cfg_name already exists in $cfg_incl_path"
-    fi
-fi
-if [ -f "$cfg_incl_path" ]; then
-    if ! grep -q "^\[respond\]$" "$cfg_incl_path"; then
-        sudo service klipper stop
-        sed -i "1i\[respond]" "$cfg_incl_path"
-        # echo "Including [respond] to $cfg_incl_path successfully complete"
-        sudo service klipper start
-    else
-        echo "Including [respond] aborted, [respond] already exists in $cfg_incl_path"
-    fi
-fi
-
-blk_path=~/printer_data/config/moonraker.conf
-if [ -f "$blk_path" ]; then
-    read -p " Do you want to install updater? (y/n): " answer
-    if [ "$answer" != "${answer#[Yy]}" ]; then
-		if ! grep -q "^\[update_manager $repo\]$" "$blk_path"; then
-			read -p " Do you want to install updater? (y/n): " answer
-			if [ "$answer" != "${answer#[Yy]}" ]; then
-			  sudo service moonraker stop
-			  sed -i "\$a \ " "$blk_path"
-			  sed -i "\$a [update_manager $repo]" "$blk_path"
-			  sed -i "\$a type: git_repo" "$blk_path"
-			  sed -i "\$a path: $repo_path" "$blk_path"
-			  sed -i "\$a origin: https://github.com/LynxCrew/$repo.git" "$blk_path"
-			  sed -i "\$a primary_branch: main" "$blk_path"
-			  sed -i "\$a managed_services: klipper" "$blk_path"
-			  # echo "Including [update_manager] to $blk_path successfully complete"
-			  sudo service moonraker start
-			else
-			  echo "Installing updater aborted"
-			fi
-		else
-			echo "Including [update_manager] aborted, [update_manager] already exists in $blk_path"
-		fi
-	fi
-fi
-
 sudo apt update
 sudo apt-get install libatlas-base-dev libopenblas-dev
 sudo pip install -r "$repo_path/"wiki/requirements.txt
